8c506a72e56a6de9c23d2d5a1e2dd33a0a3d1c00
junilhwang <junil.h@kakao.com>
2025-08-29 21:54:52 +0900

    test: 개발서버에 대한 테스트 추가

--- e2e/e2e-advanced.spec.ts
+++ e2e/e2e-advanced.spec.ts
@@ -1,8 +1,10 @@
-import { createCSRTest, createSSGTest, createSSRTest } from "./createTests";
 import test from "@playwright/test";
+import { createCSRTest, createSSGTest, createSSRTest } from "./createTests";
 
 test.describe("E2E Test > 심화과제 (React)", () => {
+  createCSRTest(`http://localhost:5175/`);
   createCSRTest(`http://localhost:4175/front_6th_chapter4-1/react/`);
+  createSSRTest(`http://localhost:5176/`);
   createSSRTest(`http://localhost:4176/front_6th_chapter4-1/react/`);
   createSSGTest(`http://localhost:4179/front_6th_chapter4-1/react/`);
 });
